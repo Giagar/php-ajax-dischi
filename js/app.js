6037aad2aa2f2620092893c5139d587f701d73cb
Gianpaolo <pao.barga@gmail.com>
2021-03-02 19:35:00 +0100

    mil 2: vue e visualizzazione con fake data funzionante; impostato json_encode in server php ma problemi: arriva pagina intera ad axios

--- js/app.js
+++ js/app.js
@@ -2,56 +2,71 @@ new Vue({
     el: "#root",
     data: {
         prova: "ok",
-        albums: [
-            {
-                "album": "New Jersey",
-                "band": "Bon Jovi",
-                "date": 1988,
-                "cover": "https://via.placeholder.com/150.jpg",
-                "genre": "rock" 
-            },
+        // albums: [
+        //     {
+        //         "album": "New Jersey",
+        //         "band": "Bon Jovi",
+        //         "date": 1988,
+        //         "cover": "https://via.placeholder.com/150.jpg",
+        //         "genre": "rock" 
+        //     },
         
-            {
-                "album": "Live at Wembley 86",
-                "band": "Queen",
-                "date": 1992,
-                "cover": "https://via.placeholder.com/150.jpg",
-                "genre": "rock" 
-            },
+        //     {
+        //         "album": "Live at Wembley 86",
+        //         "band": "Queen",
+        //         "date": 1992,
+        //         "cover": "https://via.placeholder.com/150.jpg",
+        //         "genre": "rock" 
+        //     },
         
-            {
-                "album": "Ten's Summoner's Tales",
-                "band": "Sting",
-                "date": 1993,
-                "cover": "https://via.placeholder.com/150.jpg",
-                "genre": "pop" 
-            },
+        //     {
+        //         "album": "Ten's Summoner's Tales",
+        //         "band": "Sting",
+        //         "date": 1993,
+        //         "cover": "https://via.placeholder.com/150.jpg",
+        //         "genre": "pop" 
+        //     },
             
-            {
-                "album": "New Jersey",
-                "band": "Bon Jovi",
-                "date": 1988,
-                "cover": "https://via.placeholder.com/150.jpg",
-                "genre": "rock" 
-            },
+        //     {
+        //         "album": "New Jersey",
+        //         "band": "Bon Jovi",
+        //         "date": 1988,
+        //         "cover": "https://via.placeholder.com/150.jpg",
+        //         "genre": "rock" 
+        //     },
         
-            {
-                "album": "Live at Wembley 86",
-                "band": "Queen",
-                "date": 1992,
-                "cover": "https://via.placeholder.com/150.jpg",
-                "genre": "rock" 
-            },
+        //     {
+        //         "album": "Live at Wembley 86",
+        //         "band": "Queen",
+        //         "date": 1992,
+        //         "cover": "https://via.placeholder.com/150.jpg",
+        //         "genre": "rock" 
+        //     },
         
-            {
-                "album": "Ten's Summoner's Tales",
-                "band": "Sting",
-                "date": 1993,
-                "cover": "https://via.placeholder.com/150.jpg",
-                "genre": "pop" 
-            },
+        //     {
+        //         "album": "Ten's Summoner's Tales",
+        //         "band": "Sting",
+        //         "date": 1993,
+        //         "cover": "https://via.placeholder.com/150.jpg",
+        //         "genre": "pop" 
+        //     },
         
-        ],
+        // ],
+        albums: [],
     },
+
+    mounted() {
+        axios
+        .get("../app/server.php")
+        .then(
+            function(resp) {
+
+                // assegna i dati recuperati a albums
+                // this.albums = resp.data;
+                console.log(resp);
+
+            }
+        )
+    }
 
 })
