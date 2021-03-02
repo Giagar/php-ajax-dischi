7fc110a07c70eed275c74d9f507499bc19f1efb9
Gianpaolo <pao.barga@gmail.com>
2021-03-02 16:20:09 +0100

    milestone 1: struttura base  e bozza dati fatti

---
+++ database/database.php
@@ -0,0 +1,19 @@
+<?php 
+
+$musicCollection = [
+    [
+        "album" => "New Jersey",
+        "band" => "Bon Jovi",
+        "date" => 1988,
+        "genre" => "rock" 
+    ],
+
+    [
+        "album" => "Live at Wembley 86",
+        "band" => "Queen",
+        "date" => 1992,
+        "genre" => "rock" 
+    ],
+
+];
+
