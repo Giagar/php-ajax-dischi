6c8a2a7e0400a0b6b0bcb80b9f957eb1ecae3877
Gianpaolo <pao.barga@gmail.com>
2021-03-02 16:43:16 +0100

    mil 1: placeholder dati fatto, struttura html e php base fatte

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-<?php echo "ok" ?>
+<?php 
+    require_once __DIR__ . "/database/database.php";
+
+    $data = $musicCollection;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music Collection</title>
+</head>
+<body>
+
+    <div class="wrapper">
+    
+        <header></header>
+        <main>
+            <?php foreach($data as $album) { ?>
+            <div class="album">
+                <?php echo "<img src=\"{$album['cover']}\" alt=\"cover\" />"; ?>
+                <div class="album-details">
+                    <h3 class="album"><?php echo $album["album"]; ?></h3>
+                    <div class="band"><?php echo $album["band"]; ?></div>
+                    <div class="date"><?php echo $album["date"]; ?></div>
+                </div>
+            </div>
+            <?php }; ?>
+
+        </main>
+
+    </div>
+
+    
+
+    
+</body>
+</html>
