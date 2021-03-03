a2984495d2ecae397f8be97d0b3b68fbdd06a59c
Gianpaolo <pao.barga@gmail.com>
2021-03-03 17:08:05 +0100

    bonus fatto: esercizio finito

--- app/server.php
+++ app/server.php
@@ -1,10 +1,31 @@
 <?php 
 
-require_once __DIR__ . "/../database/database.php";
+include __DIR__ . "/../database/database.php";
 
 $albums = $musicCollection;
+// $genre = $_GET["genre"];
 
-// var_dump($_GET);
+function filterByGenre($albums, $chosenGenre) {
+    $filteredCollection = [];
+
+    if($chosenGenre === "all") {
+        $filteredCollection = $albums;
+    } else {
+        foreach($albums as $album) {
+            if($album["genre"] === $chosenGenre) {
+                $filteredCollection[] = $album;
+            }
+        }
+    }
+
+    return $filteredCollection;
+}
+
+$selectedGenre = $_GET["genre"];
+
+if(!empty($selectedGenre)) {
+    $albums = filterByGenre($albums, $selectedGenre);
+}
 
 
 header("Content-Type: applications/json");
